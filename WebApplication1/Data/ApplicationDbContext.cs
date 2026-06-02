7a5956dd9a9063bed2e0b693ba4a47dfc2edf50c
onelovveer <andronchik665@gmail.com>
2026-06-02 19:31:35 +0300

    Update ApplicationDbContext.cs

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -19,14 +19,6 @@ public class ApplicationDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-            // 👇 ДОБАВЬТЕ ЭТОТ БЛОК (решает проблему с датами) 👇
-    modelBuilder.ConfigureConventions(config =>
-    {
-        config.Properties<DateTime>()
-              .HaveColumnType("timestamp without time zone");
-    });
-    // 👆 КОНЕЦ БЛОКА 👆
-
         const string dbo = "dbo";
 
         modelBuilder.Entity<User>(e =>
